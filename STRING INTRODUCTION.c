5a7d13f3e4598afea297a08bba7e7f322d9d40c6
hasiniarimanda182-code <hasiniarimanda182@gmail.com>
2025-12-04 13:21:32 +0530

    Add files via upload

---
+++ STRING INTRODUCTION.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char name[10],full_name[20],Address[30];
+	printf("Enter your name:");
+	scanf("%s",name);
+	printf("%s",name);
+	getchar();
+	printf("Enter full name:");
+	scanf(" %[^\n]",full_name);
+	printf("\n%s",full_name);
+}
