26957cb371b579285e2963a4403b72f7425a7c60
hasiniarimanda182-code <hasiniarimanda182@gmail.com>
2025-12-05 10:52:08 +0530

    Add files via upload

---
+++ INCREMENT AND DECREMENT IN POINTERS.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int *p = &a;
+	printf("address of a :%p\n",&a);
+	printf("address by using pointer:%p\n",p);
+	p++;
+	printf("after increment address is :%p\n",p);
+	p--;
+	printf("after decrement address is :%p\n",p);
+}
